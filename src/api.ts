59b9ad129f105efcc7d3ea6fe28ca4132d75395d
Rasmus <66802136+rasmus234@users.noreply.github.com>
2021-12-13 17:38:48 +0100

    change url

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import {Employee, EmployeeCake, EmployeeLunch, Item, LunchMenu} from "./models";
 const baseUrlRemote = "https://canteenapi.herokuapp.com/";
 const baseUrlLocal = "https://localhost:7106/";
 
-export const baseUrl = baseUrlLocal;
+export const baseUrl = baseUrlRemote;
 
 
 export async function loginWithPassword(password: string): Promise<Employee> {
@@ -51,7 +51,7 @@ export async function getMenuItems(): Promise<Item[]> {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + currentEmployee.token
+            "Authorization": "Bearer " + currentEmployee?.token
         },
     })
         .then(data => data.json())
@@ -82,7 +82,7 @@ export async function getCurrentMenu(): Promise<LunchMenu> {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + currentEmployee.token
+            "Authorization": "Bearer " + currentEmployee?.token
         }
     }).then(value => response = value.json());
     return response;
