8522f4bf213f40c32450593a739f879f83b862c8
rasmus234 <rasmussonntag@gmail.com>
2021-12-13 15:01:15 +0100

    add library to scan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { loginWithPassword } from "./api";
 
+// @ts-ignore
+import onScan from "onscan.js";
 
 export {}
+
+// onScan.attachTo(document)
+//
+// // @ts-ignore
+// document.addEventListener('scan', (sScancode, iQuantity) =>{
+//     alert(iQuantity + 'x ' + sScancode);
+// });
+
 const password = prompt("Enter password");
-
-
 sessionStorage.setItem("password", password);
 location.href = "/canteen.html";
+
+
