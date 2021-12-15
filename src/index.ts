0cf9765fcde5125778cefb226617b090c357d69e
BambusBo <mortenk98@gmail.com>
2021-12-15 14:26:40 +0100

    m

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,31 @@ import onScan from "onscan.js";
 
 export {}
 
- onScan.attachTo(document)
+ onScan.attachTo(document,
+     {
+         onScan: (data) => {
+             console.log(data);
+         }
+     }
+ );
 
  // @ts-ignore
  document.addEventListener('scan', (sScancode, iQuantity) =>{
      
      let pass = sScancode.detail.scanCode as string;
-     
      pass = pass.trim()
-     
-     
      console.log(pass);
-
      
      sessionStorage.setItem("password", pass);
      location.href = "/canteen.html";
  });
+ 
+
+ function simulateScan() {
+     onScan.simulate(document,"00017721093580");
+ }
+ 
+ simulateScan()
 
 /*
 const password = prompt("Enter password");
